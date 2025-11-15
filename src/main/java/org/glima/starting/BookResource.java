eb42426ab872683f767d6bcf9237a52328943ba5
VitorLima <vitorliima18@gmail.com>
2025-11-15 11:42:56 -0300

    feature: Implementando primeiros endpoints com quarkus, retornando lista de livros, contagem de livros e livros por id

--- src/main/java/org/glima/starting/BookResource.java
+++ src/main/java/org/glima/starting/BookResource.java
@@ -3,15 +3,40 @@ package org.glima.starting;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+import java.util.Optional;
 
 @Path("/api/books")
 public class BookResource {
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Book> getAllBooks() {
+        return List.of(
+                new Book(1, "Understanting Quarkus", "Antonio Goncalves", 2020, "IT"),
+                new Book(2, "Practsing Quarkus", "Antonio Goncalves", 2020, "IT"),
+                new Book(3, "Effective Java", "Josh Blocj", 2001, "IT"),
+                new Book(3, "Thinking in Java", "Bruce Eckel", 1998, "IT")
+        );
+    }
+
+    @GET
+    @Path("/count")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public int countAllBooks() {
+
+        return getAllBooks().size();
+    }
+
+    @GET
+    @Path("{id}")
+    public Optional<Book> getBook(@PathParam("id") int id) {
+
+        return getAllBooks().stream()
+                .filter(book -> book.id == id).findFirst();
     }
 }
